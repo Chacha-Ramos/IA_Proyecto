2d507e4fccd7d0decfcd07394c311fec01c65cce
Chacha-Ramos <chavaramos.ipn@gmail.com>
2021-01-16 10:33:00 -0600

    Progress

--- Pruebas/prueba.c
+++ Pruebas/prueba.c
@@ -35,38 +35,12 @@ int main()
   bmpInfoHeader info;
   unsigned char *img;
 
-  img=LoadBMP("imagen.png", &info);
+  img=LoadBMP("imagen.bmp", &info);
   DisplayInfo(&info);
-  TextDisplay(&info, img);
 
   return 0;
 }
 
-void TextDisplay(bmpInfoHeader *info, unsigned char *img)
-{
-  int x, y;
-  /* Reducimos la resolución vertical y horizontal para que la imagen entre en pantalla */
-  static const int reduccionX=6, reduccionY=4;
-  /* Si la componente supera el umbral, el color se marcará como 1. */
-  static const int umbral=90;
-  /* Asignamos caracteres a los colores en pantalla */
-  static unsigned char colores[9]=" bgfrRGB";
-  int r,g,b;
-
-  /* Dibujamos la imagen */
-  for (y=info->height; y>0; y-=reduccionY)
-    {
-      for (x=0; x<info->width; x+=reduccionX)
-    {
-      b=(img[3*(x+y*info->width)]>umbral);
-      g=(img[3*(x+y*info->width)+1]>umbral);
-      r=(img[3*(x+y*info->width)+2]>umbral);
-
-      printf("%c", colores[b+g*2+r*4]);
-    }
-      printf("\n");
-    }
-}
 
 unsigned char *LoadBMP(char *filename, bmpInfoHeader *bInfoHeader)
 {
@@ -114,13 +88,13 @@ unsigned char *LoadBMP(char *filename, bmpInfoHeader *bInfoHeader)
 
 void DisplayInfo(bmpInfoHeader *info)
 {
-  printf("Tamaño de la cabecera: %u\n", info->headersize);
+  printf("Tamano de la cabecera: %u\n", info->headersize);
   printf("Anchura: %d\n", info->width);
   printf("Altura: %d\n", info->height);
   printf("Planos (1): %d\n", info->planes);
   printf("Bits por pixel: %d\n", info->bpp);
   printf("Compresión: %d\n", info->compress);
-  printf("Tamaño de datos de imagen: %u\n", info->imgsize);
+  printf("Tamano de datos de imagen: %u\n", info->imgsize);
   printf("Resolucón horizontal: %u\n", info->bpmx);
   printf("Resolucón vertical: %u\n", info->bpmy);
   printf("Colores en paleta: %d\n", info->colors);
